976d9d7217ae9aebe5fef0c0383b17c40abd4b6b
Zitao Xiong <caoer115@gmail.com>
2024-08-24 11:33:06 +0800

    feat: add NFT Vault

---
+++ src/NFTVault.sol
@@ -0,0 +1,115 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "@openzeppelin/contracts/interfaces/IERC1155Receiver.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/interfaces/IERC165.sol";
+
+contract NFTVault is AccessControl, IERC721Receiver, IERC1155Receiver {
+    bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
+
+    event ERC721Received(
+        address indexed operator,
+        address indexed from,
+        uint256 indexed tokenId,
+        bytes data
+    );
+    event ERC1155Received(
+        address indexed operator,
+        address indexed from,
+        uint256 indexed id,
+        uint256 value,
+        bytes data
+    );
+    event ERC1155BatchReceived(
+        address indexed operator,
+        address indexed from,
+        uint256[] ids,
+        uint256[] values,
+        bytes data
+    );
+
+    constructor() {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MANAGER_ROLE, msg.sender);
+    }
+
+    function sendERC721(
+        IERC721 token,
+        address to,
+        uint256 tokenId
+    ) external onlyRole(MANAGER_ROLE) {
+        token.safeTransferFrom(address(this), to, tokenId);
+    }
+
+    function sendERC1155(
+        IERC1155 token,
+        address to,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) external onlyRole(MANAGER_ROLE) {
+        token.safeTransferFrom(address(this), to, id, amount, data);
+    }
+
+    function batchSendERC1155(
+        IERC1155 token,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) external onlyRole(MANAGER_ROLE) {
+        token.safeBatchTransferFrom(address(this), to, ids, amounts, data);
+    }
+
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes memory data
+    ) public virtual override returns (bytes4) {
+        emit ERC721Received(operator, from, tokenId, data);
+        return this.onERC721Received.selector;
+    }
+
+    function onERC1155Received(
+        address operator,
+        address from,
+        uint256 id,
+        uint256 value,
+        bytes memory data
+    ) public virtual override returns (bytes4) {
+        emit ERC1155Received(operator, from, id, value, data);
+        return this.onERC1155Received.selector;
+    }
+
+    function onERC1155BatchReceived(
+        address operator,
+        address from,
+        uint256[] memory ids,
+        uint256[] memory values,
+        bytes memory data
+    ) public virtual override returns (bytes4) {
+        emit ERC1155BatchReceived(operator, from, ids, values, data);
+        return this.onERC1155BatchReceived.selector;
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view virtual override(AccessControl, IERC165) returns (bool) {
+        return
+            interfaceId == type(IERC721Receiver).interfaceId ||
+            interfaceId == type(IERC1155Receiver).interfaceId ||
+            super.supportsInterface(interfaceId);
+    }
+
+    function grantManagerRole(address account) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        grantRole(MANAGER_ROLE, account);
+    }
+
+    function revokeManagerRole(address account) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        revokeRole(MANAGER_ROLE, account);
+    }
+}
---
+++ test/NFTVault.t.sol
@@ -0,0 +1,144 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+import "../src/NFTVault.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+contract MockERC721 is ERC721 {
+    constructor() ERC721("MockERC721", "M721") {}
+
+    function mint(address to, uint256 tokenId) public {
+        _mint(to, tokenId);
+    }
+}
+
+contract MockERC1155 is ERC1155 {
+    constructor() ERC1155("") {}
+
+    function mint(address to, uint256 id, uint256 amount) public {
+        _mint(to, id, amount, "");
+    }
+}
+
+contract NFTVaultTest is Test {
+    NFTVault public vault;
+    MockERC721 public mockERC721;
+    MockERC1155 public mockERC1155;
+    address public admin;
+    address public manager;
+    address public user;
+
+    function setUp() public {
+        admin = address(this);
+        manager = address(0x1);
+        user = address(0x2);
+
+        vault = new NFTVault();
+        mockERC721 = new MockERC721();
+        mockERC1155 = new MockERC1155();
+
+        vm.prank(manager);
+        vault.grantRole(vault.MANAGER_ROLE(), manager);
+    }
+
+    function testConstructor() public {
+        assertTrue(vault.hasRole(vault.DEFAULT_ADMIN_ROLE(), admin));
+        assertTrue(vault.hasRole(vault.MANAGER_ROLE(), admin));
+    }
+
+    function testGrantAndRevokeManagerRole() public {
+        address newManager = address(0x3);
+        
+        vault.grantManagerRole(newManager);
+        assertTrue(vault.hasRole(vault.MANAGER_ROLE(), newManager));
+
+        vault.revokeManagerRole(newManager);
+        assertFalse(vault.hasRole(vault.MANAGER_ROLE(), newManager));
+    }
+
+    function testSendERC721() public {
+        uint256 tokenId = 1;
+        mockERC721.mint(address(vault), tokenId);
+
+        vm.prank(manager);
+        vault.sendERC721(IERC721(address(mockERC721)), user, tokenId);
+
+        assertEq(mockERC721.ownerOf(tokenId), user);
+    }
+
+    function testSendERC1155() public {
+        uint256 tokenId = 1;
+        uint256 amount = 10;
+        mockERC1155.mint(address(vault), tokenId, amount);
+
+        vm.prank(manager);
+        vault.sendERC1155(IERC1155(address(mockERC1155)), user, tokenId, amount, "");
+
+        assertEq(mockERC1155.balanceOf(user, tokenId), amount);
+    }
+
+    function testBatchSendERC1155() public {
+        uint256[] memory ids = new uint256[](2);
+        uint256[] memory amounts = new uint256[](2);
+        ids[0] = 1;
+        ids[1] = 2;
+        amounts[0] = 10;
+        amounts[1] = 20;
+
+        mockERC1155.mint(address(vault), ids[0], amounts[0]);
+        mockERC1155.mint(address(vault), ids[1], amounts[1]);
+
+        vm.prank(manager);
+        vault.batchSendERC1155(IERC1155(address(mockERC1155)), user, ids, amounts, "");
+
+        assertEq(mockERC1155.balanceOf(user, ids[0]), amounts[0]);
+        assertEq(mockERC1155.balanceOf(user, ids[1]), amounts[1]);
+    }
+
+    function testOnERC721Received() public {
+        uint256 tokenId = 1;
+        mockERC721.mint(user, tokenId);
+
+        vm.prank(user);
+        mockERC721.safeTransferFrom(user, address(vault), tokenId);
+
+        assertEq(mockERC721.ownerOf(tokenId), address(vault));
+    }
+
+    function testOnERC1155Received() public {
+        uint256 tokenId = 1;
+        uint256 amount = 10;
+        mockERC1155.mint(user, tokenId, amount);
+
+        vm.prank(user);
+        mockERC1155.safeTransferFrom(user, address(vault), tokenId, amount, "");
+
+        assertEq(mockERC1155.balanceOf(address(vault), tokenId), amount);
+    }
+
+    function testOnERC1155BatchReceived() public {
+        uint256[] memory ids = new uint256[](2);
+        uint256[] memory amounts = new uint256[](2);
+        ids[0] = 1;
+        ids[1] = 2;
+        amounts[0] = 10;
+        amounts[1] = 20;
+
+        mockERC1155.mint(user, ids[0], amounts[0]);
+        mockERC1155.mint(user, ids[1], amounts[1]);
+
+        vm.prank(user);
+        mockERC1155.safeBatchTransferFrom(user, address(vault), ids, amounts, "");
+
+        assertEq(mockERC1155.balanceOf(address(vault), ids[0]), amounts[0]);
+        assertEq(mockERC1155.balanceOf(address(vault), ids[1]), amounts[1]);
+    }
+
+    function testSupportsInterface() public view {
+        assertTrue(vault.supportsInterface(type(IERC721Receiver).interfaceId));
+        assertTrue(vault.supportsInterface(type(IERC1155Receiver).interfaceId));
+        assertTrue(vault.supportsInterface(type(IERC165).interfaceId));
+    }
+}
