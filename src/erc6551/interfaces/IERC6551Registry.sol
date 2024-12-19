a4825f55f5f4e989f4b53868afab26354caba488
Zitao Xiong <caoer115@gmail.com>
2024-12-19 20:11:55 +0800

    chore: format files

--- src/erc6551/interfaces/IERC6551Registry.sol
+++ src/erc6551/interfaces/IERC6551Registry.sol
@@ -2,29 +2,21 @@
 pragma solidity ^0.8.0;
 
 interface IERC6551Registry {
-    event AccountCreated(
-        address account,
-        address implementation,
-        uint256 chainId,
-        address tokenContract,
-        uint256 tokenId,
-        uint256 salt
-    );
+  event AccountCreated(
+    address account, address implementation, uint256 chainId, address tokenContract, uint256 tokenId, uint256 salt
+  );
 
-    function createAccount(
-        address implementation,
-        uint256 chainId,
-        address tokenContract,
-        uint256 tokenId,
-        uint256 seed,
-        bytes calldata initData
-    ) external returns (address);
+  function createAccount(
+    address implementation,
+    uint256 chainId,
+    address tokenContract,
+    uint256 tokenId,
+    uint256 seed,
+    bytes calldata initData
+  ) external returns (address);
 
-    function account(
-        address implementation,
-        uint256 chainId,
-        address tokenContract,
-        uint256 tokenId,
-        uint256 salt
-    ) external view returns (address);
+  function account(address implementation, uint256 chainId, address tokenContract, uint256 tokenId, uint256 salt)
+    external
+    view
+    returns (address);
 }
